c13e807934dd0285fdf3b86e186f732458602679
rfielding <rfielding@pop-os.localdomain>
2021-02-07 20:28:49 -0500

    include shards

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-  "fmt"
-  "crypto/elliptic"
-  "crypto/rand"
-  "crypto/sha256"
-  "encoding/json"
-  "encoding/hex"
-  "math/big"
-  "bytes"
+	"bytes"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"math/big"
 )
 
 /*
   Experiment in an alternative "blockchain-style" database.
   Current blockchains (ie: hashpointered data structures)
-  are integrity checking the EVENT STREAM that creates 
+  are integrity checking the EVENT STREAM that creates
   the database, rather than integrity checking the DATA.
 
   You should get the same signature on the database if
@@ -64,59 +64,82 @@ import (
 
   Insert and Remove events should be maintained in a
   list from a given state.  This is used to efficiently diff two copies.
- */
+*/
+
+type Reference struct {
+	Shard Shard
+	Id    Id
+}
 
 type DataRecord struct {
-	Id int64
-	TTL int64
-	Refs map[string]int64
-	Name string
+	Shard   Shard
+	Id      Id
+	TTL     int64
+	Refs    map[string]Reference
+	Name    string
 	Strings map[string]string
 }
 
 func AsJson(v interface{}) string {
-	s, err := json.MarshalIndent(v,"","  ")
+	s, err := json.MarshalIndent(v, "", "  ")
 	if err != nil {
 		panic(fmt.Sprintf("cannot marshal data: %v", err))
 	}
 	return string(s)
 }
 
-type Db struct {
-	Data map[int64]*DataRecord
-	KeyPair KeyPair
+type Id int64
+type Shard int64
+
+type Point struct {
 	X *big.Int
 	Y *big.Int
 }
 
-func NewDB() (*Db,error) {
-	s,x,y, err := elliptic.GenerateKey(elliptic.P521(), rand.Reader)
+type Db struct {
+	Shard   Shard
+	Data    map[Shard]map[Id]*DataRecord
+	State   map[Shard]Point
+	KeyPair KeyPair
+}
+
+func NewDB(shard Shard) (*Db, error) {
+	s, x, y, err := elliptic.GenerateKey(elliptic.P521(), rand.Reader)
 	if err != nil {
 		return nil, err
 	}
 	kp := KeyPair{
-		Curve: elliptic.P521(),
+		Curve:  elliptic.P521(),
 		Secret: s,
-		X: x,
-		Y: y,
+		X:      x,
+		Y:      y,
 	}
 	xInit, yInit := kp.Curve.ScalarBaseMult(nil)
-	return &Db{
-		Data: make(map[int64]*DataRecord,0),
-		KeyPair: kp,
+	data := make(map[Shard]map[Id]*DataRecord)
+	data[shard] = make(map[Id]*DataRecord)
+	states := make(map[Shard]Point)
+	states[shard] = Point{
 		X: xInit,
 		Y: yInit,
-	},nil
+	}
+	return &Db{
+		Shard:   shard,
+		Data:    data,
+		KeyPair: kp,
+		State:   states,
+	}, nil
 }
 
 func (db *Db) sum(h []byte, neg bool) {
-	x1,y1 := db.KeyPair.Curve.ScalarBaseMult(h)
+	x1, y1 := db.KeyPair.Curve.ScalarBaseMult(h)
 	if neg {
 		y1 = new(big.Int).Neg(y1)
 	}
-	x2,y2 := db.KeyPair.Curve.Add(db.X,db.Y, x1,y1)
-	db.X = x2
-	db.Y = y2
+	p := db.State[db.Shard]
+	x2, y2 := db.KeyPair.Curve.Add(p.X, p.Y, x1, y1)
+	p.X = x2
+	p.Y = y2
+	db.State[db.Shard] = p
 }
 
 // Insert the record if it does not exist
@@ -124,7 +147,7 @@ func (db *Db) sum(h []byte, neg bool) {
 // value
 func (db *Db) Insert(v *DataRecord) error {
 	var hExisting []byte
-	vExisting,ok := db.Data[v.Id]
+	vExisting, ok := db.Data[db.Shard][v.Id]
 	if ok {
 		jExisting := AsJson(vExisting)
 		hExisting = sha256.New().Sum([]byte(jExisting))
@@ -138,61 +161,65 @@ func (db *Db) Insert(v *DataRecord) error {
 	// Remove and re-add
 	if len(hExisting) > 0 {
 		db.Remove(v.Id)
-	}	
+	}
 	db.sum(h, false)
-	db.Data[v.Id] = v 
+	db.Data[db.Shard][v.Id] = v
 	return nil
 }
 
 // Remove the record if it is there
 // or do nothing if it is not there
-func (db *Db) Remove(id int64) (*DataRecord,error) {
-	v, ok := db.Data[id]
+func (db *Db) Remove(id Id) (*DataRecord, error) {
+	v, ok := db.Data[db.Shard][id]
 	if !ok {
 		return nil, nil
 	}
 	j := AsJson(v)
 	h := sha256.New().Sum([]byte(j))
 	db.sum(h, true)
-	delete(db.Data, id)
-	return v,nil
+	delete(db.Data[db.Shard], id)
+	return v, nil
 }
 
 func (db *Db) Checksum() string {
 	return fmt.Sprintf(
-		"%s,%s", 
-		hex.EncodeToString(db.X.Bytes()),
-		hex.EncodeToString(db.Y.Bytes()),
+		"%s,%s",
+		hex.EncodeToString(db.State[db.Shard].X.Bytes()),
+		hex.EncodeToString(db.State[db.Shard].Y.Bytes()),
 	)
 }
 
 type KeyPair struct {
-	Curve elliptic.Curve
+	Curve  elliptic.Curve
 	Secret []byte
-	X *big.Int
-	Y *big.Int
+	X      *big.Int
+	Y      *big.Int
 }
 
 func main() {
-	db, err := NewDB()
+	db, err := NewDB(42)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("initial checksum: %s\n",db.Checksum())
+	fmt.Printf("initial checksum: %s\n\n", db.Checksum())
 	db.Insert(&DataRecord{
-		Id: 1,
-		TTL: 20,
+		Shard: db.Shard,
+		Id:   1,
+		TTL:  20,
 		Name: "initial",
 	})
-	fmt.Printf("%s\n",db.Checksum())
+	fmt.Printf("id1: %s\n\n", db.Checksum())
 	db.Insert(&DataRecord{
-		Id: 2,
-		TTL: 21,
+		Shard: db.Shard,
+		Id:   2,
+		TTL:  21,
 		Name: "secondary",
 	})
-	fmt.Printf("%s\n",db.Checksum())
+	fmt.Printf("id1 + id2: %s\n\n", db.Checksum())
+
 	db.Remove(2)
-	fmt.Printf("%s\n",db.Checksum())
+	fmt.Printf("id1: %s\n\n", db.Checksum())
+
 	db.Remove(1)
-	fmt.Printf("%s\n",db.Checksum())
+	fmt.Printf("empty checksum: %s\n\n", db.Checksum())
 }
