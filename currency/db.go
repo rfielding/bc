98eef9507cfd402a3a883673fd6351fa9dc92882
rfielding <rfielding@pop-os.localdomain>
2021-02-14 06:47:28 -0500

    more work towards a working currency POC

--- currency/db.go
+++ currency/db.go
@@ -5,16 +5,27 @@ import (
 	"fmt"
 )
 
-// The database stores a bunch of records
+// A multiple-input and multiple output account-based system:
+//
+//  (alice: 5, bob: 6) send: (charles: 10, taxman: 1)
+//
 type Db interface {
+	// Try to insert transactions into the chain
+	// Pushing onto This() receipt.
 	PushTransaction(txn Transaction) (Receipt, ErrTransaction)
+	// Move around the chain
 	RePush(i int) (Receipt, ErrTransaction)
 	PeekNext() []Receipt
 	PopTransaction() bool
 	CanPopTransaction() bool
+	// Locations.  Will need help navigating to longest chain.
 	Genesis() Receipt
 	This() Receipt
-	AsBank(k PublicKey) // hack to deal with accounts with negative balances, like treasuries
+	// Stupid hack to deal with accounts with negative balances, like treasuries
+	// Something like proof-of-work will be needed to remove the Treasury hack
+	AsBank(k PublicKey)
+	// allow for partially signed transactions to go out,
+	// so that everybody that needs to sign CAN sign.
 	Sign(k *ecdsa.PrivateKey, txn *Transaction, i int) *Transaction
 }
 
